3e521f311d42fc1fafb892ba728f19ac40fb93cb
ZzjeampierZz <76917992+ZzjeampierZz@users.noreply.github.com>
2021-08-05 17:30:12 -0500

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,119 @@
+export const skills = 
+[
+    {
+        skill:'Javascrip',
+        percentage:'50'
+    },
+    {
+        skill:'Java',
+        percentage:'70'
+    },
+    {
+        skill:'PHP',
+        percentage:'100'
+ 
+   },
+   {
+    skill:'Python',
+    percentage:'80'
+   },
+    {
+    skill:'MySql',
+    percentage:'100'
+   },   
+    {
+    skill:'Sql',
+    percentage:'100'
+},
+{
+    skill:'Mongodb',
+    percentage:'100'
+},
+{
+    skill:'Javascrip',
+    percentage:'100'
+}
+]
+export const experinces = 
+
+[
+    {
+        title:'Software delover at Microsoft',
+        description:"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ips",
+        from: 2010,
+        to:2005
+    },
+    {
+        title:'Software delover at Microsoft',
+        description:"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc",
+        from: 2010,
+        to:2005
+    },
+    {
+        title:'Software delover at Microsoft',
+        description:"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc",
+        from: 2010,
+        to:2005
+    },
+    {
+        title:'Software delover at Microsoft',
+        description:"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc",
+        from: 2010,
+        to:2005
+    },
+    {
+        title:'Software delover at Microsoft',
+        description:"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc",
+        from: 2010,
+        to:2005
+    }
+]
+export const projects=[
+
+    {
+        name:"Awesome website 1",
+        description:"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium volfhggfh hthtt tht tht", 
+        Image:"4.png"
+     },
+     {
+        name:"Awesome website 2",
+        description:"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium volfhggfh hthtt tht tht", 
+        Image:"5.png"
+     },
+     {
+        name:"Awesome website 3",
+        description:"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium volfhggfh hthtt tht tht", 
+        Image:"6.png"
+     },    {
+        name:"Awesome website 4",
+        description:"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium volfhggfh hthtt tht tht", 
+        Image:"7.png"
+     },  {
+        name:"Awesome website 5",
+        description:"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium volfhggfh hthtt tht tht", 
+        Image:"foto6.jpg"
+     },    {
+        name:"Awesome website 6",
+        description:"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium volfhggfh hthtt tht tht", 
+        Image:"foto7.jpg"
+     },
+
+]
+export const posts = [
+    {
+    title: "React",
+    content:"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system,",
+    imagen:'https://www.syntonize.com/wp-content/uploads/2021/02/React-Syntonize.png'
+},
+{
+    title: "Angular",
+    content:"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system,",
+    imagen:'https://victorroblesweb.es/wp-content/uploads/2017/04/angular4.png'
+},
+{
+    title: "Vue.js",
+    content:"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system,",
+    imagen:'https://miro.medium.com/max/512/1*b1C8z2YWcBm9uadnfcjLUQ.jpeg'
+}
+
+]
